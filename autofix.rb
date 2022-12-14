2020342c2a690a09ed752f28a4a921434151eee8
Syed Faraaz Ahmad <113512303+faraaz-deepsource@users.noreply.github.com>
2022-12-14 10:59:11 +0530

    Create autofix.rb

---
+++ autofix.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+a = {'hello' => 'world', "asss" => BigDecimal.new(123.456, 3)}
+puts a
+
+begin
+  do_something
+end while some_condition
+
+b = [1, 2, 3, 4, 5]
+puts b
+
+OpenSSL::Cipher::AES.new(128, :GCM)
